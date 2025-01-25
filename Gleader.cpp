2c9a0cb4d35b505cd9828ea192a9b797810a2294
FPValentino <202311194@fit.edu.ph>
2025-01-25 22:20:24 +0800

    Incomplete

--- Gleader.cpp
+++ Gleader.cpp
@@ -5,17 +5,20 @@ Priority Queue implementation - added similar max heap funtionality.
 TO DO
 Link ARRAY and LINK LIST (goods na ata?)
 DISPLAY DESIGN
-FILE HANDLING
+FILE HANDLING (Oks naren ata? I mean pwede lagyan ng design ung pag save sa file pero nag fufunction na)
 ERROR HANDLING for inputs
 
 */
 
 
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 const int MAX = 10;
-int LEAD[MAX]; //array for top 10 leaderboard
+int LEADScore[MAX]; //array for top 10 leaderboard
+string LEADName[MAX];
 
 //linked list
 struct Node {
@@ -28,8 +31,11 @@ struct Node {
 };
 Node* head = NULL;
 
+//prototypes
 void push(string name, int score);
 void displayScore();//temporary
+void saveFile();
+void loadFile();
 
 //prio queue implementation, prio of highest number is always 1, incrementing.
 void push(string name, int score){ 
@@ -94,7 +100,8 @@ void displayScore(){
 		p = head;
 		while (p!=NULL){
 			//saving top 10 scores to array (di pako certain kung ano restrictions sa top 10)
-			LEAD[i] = p->score;
+			LEADScore[i] = p->score;
+			LEADName[i] = p->name;
 				if (count == 10){
 					break;
 				}
@@ -108,7 +115,7 @@ void displayScore(){
 	i = 1;
 		for (int j = 0; j < MAX; j++){
 			if (p){
-				cout << "Rank "<< rank << " " << p->name << " (" << LEAD[j] << ")" << endl;
+				cout << "Rank "<< rank << " " << LEADName[j] << " (" << LEADScore[j] << ")" << endl;
 					if (p && p->next->score == p->score){
 					 //skip rank increment
 					} else{
@@ -121,34 +128,49 @@ void displayScore(){
 				break;
 			}
 		}
+		saveFile();
+}
+
+void saveFile(){
+	ofstream file("leaderboard.txt");
+	for(int i = 0; i<MAX; i++){
+		file << LEADName[i] << " " << LEADScore[i] << endl;
+	}
+	file.close();
+
+	ofstream rec("record.txt");
+	Node* tmp = head;
+	while(tmp != NULL){
+		rec << tmp->name << " " << tmp->score << endl;
+		tmp = tmp->next;
+	}
+	rec.close();
+}
+
+void loadFile(){
+	ifstream file("leaderboard.txt");
+	for(int i = 0; i<MAX; i++){
+		file >> LEADName[i] >> LEADScore[i];
+	}
+	file.close();
+
+	ifstream rec("record.txt");
+	string name;
+	int score;
+
+	while (rec >> name >> score) {
+		push(name, score);
+	}
+
+	rec.close();
+
 }
 
 int main(){
 	//tests
-	push("Jule", 5); // prio = 1
-	push("Fac", 3);
-	push("You", 1);
-	push("C", 5);
-	push("Ferd", 6);
-    push("Ferd", 21);
-    push("Ferd", 18);
-    push("Ferd", 2);
-    push("Ferd", 7);
-	push("Test", 12);
-	push("Hadj", 16);
-	push("Des", 31);
-	push("Charl", 101);
-	push("Carl", 101);
-	push("Adrian",32);
-	push("Sup", 4);
-	push("Hello", 101);
-	push("Its", 101);
-	push("Me", 16);
-
+	loadFile();
 	displayScore();
 
-	push("Charl", 102);
-	displayScore();
 	return 0;
 }
 
