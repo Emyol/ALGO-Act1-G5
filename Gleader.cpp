16163f736d553af003e11606d2e5271d84eac713
FPValentino <202311194@fit.edu.ph>
2025-01-25 20:41:33 +0800

    Incomplete

--- Gleader.cpp
+++ Gleader.cpp
@@ -3,7 +3,7 @@ Linklist - add record function
 Priority Queue implementation - added similar max heap funtionality.
 
 TO DO
-Link ARRAY and LINK LIST
+Link ARRAY and LINK LIST (goods na ata?)
 DISPLAY DESIGN
 FILE HANDLING
 
@@ -88,11 +88,34 @@ if(!head){
 
 //for testing
 void displayScore(){
+	int count = 1, i = 0, rank = 1;
 	Node *p;
 		p = head;
 		while (p!=NULL){
-			cout << p->name << " " << p->score << " " << p->prio << endl;
-				p=p->next;
+			//saving top 10 scores to array (di pako certain kung ano restrictions sa top 10)
+			LEAD[i] = p->score;
+				if (count == 10){
+					break;
+				}
+			++i;
+			++count;
+			p = p->next;
+		}
+
+	cout << "ARRAY Content: \n";
+		p = head;
+		count = 0;
+		for (int j = 0; j < MAX; j++){
+			if (p){
+				cout << "Rank "<< rank << " " << p->name << " (" << LEAD[j] << ")" <<endl;
+					if (p && p->next->score == p->score){
+						rank--;
+					}
+				p = p->next;
+				rank++;
+			} else{
+				break;
+			}
 		}
 }
 
@@ -107,6 +130,16 @@ int main(){
     addRecord("Ferd", 18);
     addRecord("Ferd", 2);
     addRecord("Ferd", 7);
+	addRecord("Test", 12);
+	addRecord("Hadj", 16);
+	addRecord("Des", 31);
+	addRecord("Charl", 81);
+	addRecord("Carl", 57);
+	addRecord("Adrian",32);
+	addRecord("Sup", 4);
+	addRecord("Hello", 101);
+	addRecord("Its", 8);
+	addRecord("Me", 10);
 
 	displayScore();
 
